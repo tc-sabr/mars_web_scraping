c459577586f98041bf0e9c7d1e1eee7654ebac26
tc-sabr <42155277+tc-sabr@users.noreply.github.com>
2018-11-14 15:49:42 -0700

    home & scrape route set

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_costa
+
+app = Flask(__name__)
+
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+@app.route('/')
+def home():
+
+    mars_dataset = mongo.db.collection.find_one()
+
+    return render_template('index.html', info=mars_dataset)
+
+@app.route('/scrape')
+def scrape():
+
+    mars_data = scrape_mars.scrape()
+
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
